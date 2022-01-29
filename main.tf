9178896b51379fc689f0311257ff562270318e4c
Farid987 <91734370+Farid987@users.noreply.github.com>
2022-01-29 22:55:31 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
       bucket = "terraformbucket30"
       encrypt = true
-      key = "terraform.tfstate"
+      key = "path/to/my/key"
       region = "us-east-1"
   }
 }
