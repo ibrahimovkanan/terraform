4f047a1c165146d04ab74ab59e51dc3d47bcf371
Farid987 <91734370+Farid987@users.noreply.github.com>
2022-01-29 22:44:25 +0400

    Add files via upload

---
+++ instance.tf
@@ -0,0 +1,73 @@
+
+resource "aws_instance" "database" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  security_groups= ["${aws_security_group.test.id}"]
+  key_name = "mytestkey"
+  subnet_id = "${aws_subnet.subnet2.id}"
+
+  connection {
+    type     = "ssh"
+    host     = self.public_ip
+    user     = "ubuntu"
+    private_key = file("/home/farid/Downloads/mytestkey.pem")
+  }
+
+
+   provisioner "file" {
+    source      = "user.sql"
+    destination = "/home/ubuntu/user.sql"
+  
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install mysql-server -y",
+      "sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf ",
+      "sudo service mysql restart",
+      "sudo mysql < user.sql"
+    ]
+  }
+}
+
+
+resource "aws_instance" "app" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  security_groups= ["${aws_security_group.test.id}"]
+  key_name = "mytestkey"
+  subnet_id = "${aws_subnet.subnet1.id}"
+
+
+  connection {
+    type     = "ssh"
+    host     = self.public_ip
+    user     = "ubuntu"
+    private_key = file("/home/farid/Downloads/mytestkey.pem")
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt upgrade -y",
+      "sudo apt install default-jre -y",
+      "sudo apt install maven -y",
+      "git clone https://github.com/Farid987/spring-petclinic.git",
+      "cd spring-petclinic",
+      "sed -i 's/0.0.0.0/${aws_instance.database.private_ip}/g' src/main/resources/application-mysql.properties",
+      "mvn spring-boot:run -Dspring-boot.run.profiles=mysql"
+    ]
+  }
+
+}
+
+output "ip" {
+  value = aws_instance.app.public_ip
+} 
+
+
+
+
+
