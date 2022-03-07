ba9ad2af6670161f7fcf8e59c2087e6c3d6432ee
Farid <91734370+Farid987@users.noreply.github.com>
2022-03-07 12:15:50 +0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,16 +1,27 @@
+provider "aws" {
+   region = "us-east-1"
+}
+
+terraform {
+   backend "s3" {
+      bucket = "terraformbucket31"  
+      key    = "gitaction/terraform.tfstate"
+      region = "us-east-1"
+   }
+}
+
+
 
 resource "aws_instance" "database" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
-  security_groups= ["${aws_security_group.test.id}"]
-  key_name = "mytestkey"
-  subnet_id = "${aws_subnet.subnet2.id}"
+  key_name = "faridkey"
 
   connection {
     type     = "ssh"
     host     = self.public_ip
     user     = "ubuntu"
-    private_key = file("mytestkey.pem")
+    private_key = file("/home/farid/Downloads/faridkey.pem")
   }
 
 
@@ -22,8 +33,7 @@ resource "aws_instance" "database" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update -y",
-      "sudo apt update -y",
+      "sudo apt update",
       "sudo apt install mysql-server -y",
       "sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf ",
       "sudo service mysql restart",
@@ -36,16 +46,14 @@ resource "aws_instance" "database" {
 resource "aws_instance" "app" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
-  security_groups= ["${aws_security_group.test.id}"]
-  key_name = "mytestkey"
-  subnet_id = "${aws_subnet.subnet1.id}"
+  key_name = "faridkey"
 
 
   connection {
     type     = "ssh"
     host     = self.public_ip
     user     = "ubuntu"
-    private_key = file("mytestkey.pem")
+    private_key = file("/home/farid/Downloads/faridkey.pem")
   }
 
 
@@ -57,16 +65,19 @@ resource "aws_instance" "app" {
       "sudo apt install maven -y",
       "git clone https://github.com/Farid987/spring-petclinic.git",
       "cd spring-petclinic",
-      "sed -i 's/0.0.0.0/${aws_instance.database.private_ip}/g' src/main/resources/application-mysql.properties",
-      "mvn spring-boot:run -Dspring-boot.run.profiles=mysql"
+      "sed -i 's/0.0.0.0/${aws_instance.database.private_ip}/g' src/main/resources/application-mysql.properties"
     ]
   }
 
 }
 
+
 output "ip" {
   value = aws_instance.app.public_ip
-} 
+}
+
+
+ 
 
 
 
